645708b24714ebceb13c805294950ff43286b292
Bogdan Volkov <116603564+Vo1koV123@users.noreply.github.com>
2023-06-27 22:47:54 +0300

    Revert "completed the task"

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,5 @@
-import coffee.order.CoffeeOrderBoard;
-import coffee.order.Order;
-
 public class Main {
     public static void main(String[] args) {
 
-                CoffeeOrderBoard board = new CoffeeOrderBoard();
-                board.add(new Order("Bogdan"));
-                board.add(new Order("Alex"));
-                board.add(new Order("Max"));
-                board.add(new Order("Denis"));
-                board.draw();
-                System.out.println("Delivering next order: " + board.deliver().getName());
-                board.draw();
-                System.out.println("Delivering order 3: " + board.deliver(3).getName());
-                board.draw();
-            }
-        }
-
-
-
+    }
+}
